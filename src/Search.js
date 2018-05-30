fc7a1f96febd9eff375bbd468946f26dd6745321
Amr A. Abdelmoez <amradel88@live.com>
2018-05-30 23:57:44 +0200

    feat: functional search page

--- src/Search.js
+++ src/Search.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
-import * as BooksAPI from './BooksAPI'
+import { Link } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
 
-import FallbackCover from "./icons/book.png";
+import FallbackCover from './icons/book.png';
 
 class Search extends Component {
-
     state = {
         books: []
-    }
+    };
 
-    componentDidMount() {
-        BooksAPI.search('biography').then(books => {
-            this.setState( {books} )
+    searchBooks(keyword) {
+        BooksAPI.search(keyword).then(books => {
+            this.setState({ books });
         });
     }
 
@@ -27,52 +26,72 @@ class Search extends Component {
                         <input
                             type="text"
                             placeholder="Search by title or author"
+                            onChange={event =>
+                                this.searchBooks(event.target.value)
+                            }
                         />
                     </div>
                 </div>
                 <div className="search-books-results">
                     <ol className="books-grid">
-                    {this.state.books.map((book) => (
-                        <li key={book.id}>
-                            <div className="book">
-                                <div className="book-top">
-                                    <div
-                                        className="book-cover"
-                                        style={{
-                                            width: 128,
-                                            height: 193,
-                                            backgroundSize: 'cover',
-                                            backgroundImage: `url(${ book.imageLinks ? book.imageLinks.thumbnail : FallbackCover })`
-                                        }}
-                                        />
-                                    <div className="book-shelf-changer">
-                                        <select>
-                                            <option value="none" disabled>
-                                                Move to...
-                                            </option>
-                                            <option value="currentlyReading">
-                                                Currently Reading
-                                            </option>
-                                            <option value="wantToRead">
-                                                Want to Read
-                                            </option>
-                                            <option value="read">Read</option>
-                                            <option value="none">None</option>
-                                        </select>
+                        {this.state.books &&
+                            this.state.books.map(book => (
+                                <li key={book.id}>
+                                {book.shelf}
+                                    <div className="book">
+                                        <div className="book-top">
+                                            <div
+                                                className="book-cover"
+                                                style={{
+                                                    width: 128,
+                                                    height: 193,
+                                                    backgroundSize: 'cover',
+                                                    backgroundImage: `url(${
+                                                        book.imageLinks
+                                                            ? book.imageLinks
+                                                                  .thumbnail
+                                                            : FallbackCover
+                                                    })`
+                                                }}
+                                            />
+                                            <div className="book-shelf-changer">
+                                                <select onChange={e => (
+                                                    BooksAPI.update(book, e.target.value)
+                                                )}>
+                                                    <option
+                                                        value="none"
+                                                        disabled
+                                                    >
+                                                        Move to...
+                                                    </option>
+                                                    <option value="currentlyReading">
+                                                        Currently Reading
+                                                    </option>
+                                                    <option value="wantToRead">
+                                                        Want to Read
+                                                    </option>
+                                                    <option value="read">
+                                                        Read
+                                                    </option>
+                                                    <option value="none">
+                                                        None
+                                                    </option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div className="book-title">
+                                            {book.title}
+                                        </div>
+                                        <div className="book-authors">
+                                            {book.authors
+                                                ? book.authors.join(`, `)
+                                                : 'Unknown'}
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="book-title">
-                                    {book.title}
-                                </div>
-                                <div className="book-authors">
-                                    {book.authors ? book.authors.join(`, `) : 'Unknown'}
-                                </div>
-                            </div>
-                        </li>
-                    ))}
+                                </li>
+                            ))}
                     </ol>
                 </div>
-
             </div>
         );
     }
